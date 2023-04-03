5d11d806c13675c37ccee64862b7727f81f90894
furu-kaggle <ituse@furu1996.tokyo>
2023-04-03 02:42:45 +0000

    Update: 2023-04-03 02:42:45

--- update.sh
+++ update.sh
@@ -22,7 +22,7 @@ echo "Last commit UUID: $commit_uuid"
 
 # Kaggle datasetの更新
 cp src/*.py src/weight/exp/
-#cd src/weight
-#cp -r exp "$commit_message"
-#rm exp/*
+cd src/weight
+cp -r exp "$commit_message"
+rm exp/*
 kaggle datasets version -m "$commit_message" -p src/weight -r tar
